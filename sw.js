899d2ed2b19a283d8fa149be6cc2a34fdf06c2e7
patniy <286756578+patniy@users.noreply.github.com>
2026-08-20 16:54:32 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
-const CACHE="olympiad-quest-v1";
+const CACHE="olympiad-quest-v3";
 const ASSETS=["./","./index.html","./questions.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener("install", e =>
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  )
+);
+
+self.addEventListener("activate", e =>
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  )
+);
+
+self.addEventListener("fetch", e =>
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+);
